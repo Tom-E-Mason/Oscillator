7cf6e3e03d181701e55b5ba87e7150f117bee718
Tom Mason <masonworldt@gmail.com>
2021-01-26 11:19:14 +0000

    Commented functions and structs.

--- UnitTests/src/OscillatorHelpers.h
+++ UnitTests/src/OscillatorHelpers.h
@@ -4,6 +4,9 @@
 #define NUM_SAMPLES_TEST 30000
 #define FLOAT_T double
 
+// ---------------------------------------------------------------------------------------
+// Instruction vectors. These are used to create the different testing conditions.
+// ---------------------------------------------------------------------------------------
 std::vector<FLOAT_T> vSampleRates{ 44100.0, 48000.0, 96000.0, 182000.0 };
 std::vector<FLOAT_T> vFrequencies{ 10 };
 std::vector<FLOAT_T> vAmplitudes{ 0.0, 0.2, 0.4, 0.6, 0.8, 1.0 };
@@ -14,6 +17,9 @@ size_t uNumSineCombinations{ vSampleRates.size() *
 std::vector<size_t> vNumHarmonics{ 2, 4, 6, 8, 10 };
 size_t uNumComplexCombinations{ uNumSineCombinations * vNumHarmonics.size() };
 
+// ---------------------------------------------------------------------------------------
+// Used by testing oscillators to generate waves.
+// ---------------------------------------------------------------------------------------
 template<typename FloatType>
 struct Tone
 {
@@ -23,18 +29,35 @@ struct Tone
     FloatType amplitude = 0.0;
 };
 
-void AddRandomFrequencies(std::vector<FLOAT_T>& vFreqs)
+// ---------------------------------------------------------------------------------------
+// Fills a vector of floats with random numbers between 20 and 20000.
+//
+// Arguments:
+//     _vFreqs - vector to fill
+//
+// Returns:
+//     void
+// ---------------------------------------------------------------------------------------
+void AddRandomFrequencies(std::vector<FLOAT_T>& _vFreqs)
 {
     std::default_random_engine rng;
     rng.seed((unsigned int)std::chrono::system_clock::now().time_since_epoch().count());
     std::uniform_real_distribution<FLOAT_T> dist;
-    for (auto& f : vFreqs)
+    for (auto& f : _vFreqs)
         f = dist(rng);
 }
 
+// ---------------------------------------------------------------------------------------
+// Takes in a SineWave object and tests it against the testing oscillator.
+//
+// Arguments:
+//     _sine - SineWave object to test
+//
+// Returns void
+// ---------------------------------------------------------------------------------------
 template <typename FloatType,
-    typename = std::enable_if_t<std::is_same_v<float, FloatType>
-    || std::is_same_v<double, FloatType>>>
+          typename = std::enable_if_t<std::is_same_v<float, FloatType>
+                                      || std::is_same_v<double, FloatType>>>
     void CheckSine(osc::SineWave<FloatType> _sine)
 {
     FloatType phase{ 0.0 };
@@ -52,7 +75,16 @@ template <typename FloatType,
 }
 
 // ---------------------------------------------------------------------------------------
-// Takes 
+// Takes a ComplexWave and a function pointer. The function should generate
+// characteristics for the testing oscillator so it can produce the correct wave to test
+// against the ComplexWave passed in.
+//
+// Arguments:
+//     _complex - ComplexWave object (currently only derived from by SquareWave)
+//     _func    - function pointer to give testing oscillator instructions
+//
+// Returns:
+//     void
 // ---------------------------------------------------------------------------------------
 template <typename FloatType,
           typename F,
@@ -86,8 +118,17 @@ template <typename FloatType,
     }
 }
 
+// ---------------------------------------------------------------------------------------
+// Fills vector of derivatives of ComplexWave with instructions for testing.
+//
+// Arguments:
+//     _vWaves - vector of ComplexWave subobjects
+//
+// Returns:
+//     void
+// ---------------------------------------------------------------------------------------
 template<typename WaveType,
-    typename = std::enable_if_t<std::is_convertible_v<WaveType*, osc::ComplexWave<FLOAT_T>*>>>
+         typename = std::enable_if_t<std::is_convertible_v<WaveType*, osc::ComplexWave<FLOAT_T>*>>>
     void CreateComplexWaveInstructions(std::vector<WaveType>& _vWaves)
 {
     _vWaves.reserve(uNumComplexCombinations);
@@ -98,6 +139,15 @@ template<typename WaveType,
                     _vWaves.push_back({ sr, f, a , n });
 }
 
+// ---------------------------------------------------------------------------------------
+// Returns the next sample from the testing oscillator.
+//
+// Arguments:
+//     _vWaveComponents - vector of Tones which each act as their own sine oscillator
+//
+// Returns:
+//     sample value
+// ---------------------------------------------------------------------------------------
 template<typename FloatType>
 FloatType ComplexWaveNextSample(std::vector<Tone<FloatType>>& _vWaveComponents)
 {
