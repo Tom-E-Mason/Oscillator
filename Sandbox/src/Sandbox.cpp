210c62cdc9e39f8112cb2da5a1a4ba4aaea9f7a1
Tom Mason <masonworldt@gmail.com>
2021-01-26 11:44:59 +0000

    Added third party olcNoiseMaker.h to test sample audio. Set up testing in main() in Sandbox.cpp.

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,12 +1,47 @@
 
-#include <iostream>
+#include <fstream>
 
+#include "olcNoiseMaker.h"
 #include "Oscillator.h"
+
+double dSampleRate = 44100.0;
+double dFreq{ 10 };
+//static osc::SquareWave<double> s(dSampleRate, dFreq, 1.0, 10);
+static osc::SineWave<double> s(dSampleRate, dFreq, 1.0);
+
+double Next(double)
+{
+    s.MultiplyFrequency(1.00001);
+    return s.NextSample();
+}
+
+void PrintWave(size_t _length)
+{
+    std::ofstream os("Samples.csv");
+    if (!os.is_open())
+    {
+        std::cout << "Failed to open file\n";
+        return;
+    }
+
+    for (size_t i{ 0 }; i < _length; ++i)
+    {
+        os << i << "," << s.NextSample() << "\n";
+        s.MultiplyFrequency(1.0001);
+    }
+}
 
 int main()
 {
-    osc::SineWave<double> s(44100.0, 440.0);
+    auto devs{ olcNoiseMaker<short>::Enumerate() };
+    olcNoiseMaker<short> nm(devs[0]);
 
-    std::cout << "Hello World!\n";
-    std::cout << s.NextSample() << "\n";
+    PrintWave((size_t)dSampleRate);
+
+    nm.SetUserFunction(Next);
+
+    while (1)
+    {
+
+    }
 }
